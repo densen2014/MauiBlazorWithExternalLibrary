9463030bcdb1faeb581eb0be1290e95ead01da75
Alex chow <zhouchuanglin@gmail.com>
2023-03-19 15:45:33 +0100

    添加项目文件。

---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿// **********************************
+// Densen Informatica 中讯科技
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com
+// **********************************
+
+namespace TestLibrary
+{
+    public class Class1
+    {
+        public static string Test()
+        {
+            Console.WriteLine("Hello World!");
+            return "xxx";
+        }
+
+    }
+}
